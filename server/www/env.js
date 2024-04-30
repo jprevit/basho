ab4d85af45106c2b48bf4d141768c20799500e2c
John Previtera <johngprevitera@gmail.com>
2024-04-30 15:01:27 -0600

    Initial spin up working

--- server/www/env.js
+++ server/www/env.js
@@ -1,3 +1,3 @@
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-xx3uaukw42oxjpp7.us.auth0.com'
+export const clientId = 'tT0AlUNf86WktlsiRBhWasSin54bs39a'
+export const audience = 'https://learning-code.com/'
