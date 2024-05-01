02365a49be1b044a6eb85c203e79d4738675aef3
John Previtera <johngprevitera@gmail.com>
2024-05-01 13:40:31 -0600

    made league model

---
+++ server/src/models/League.js
@@ -0,0 +1,20 @@
+import { Schema } from "mongoose";
+
+export const LeagueSchema = new Schema({
+    creatorId: { type: Schema.ObjectId, required: true, ref: 'Account' },
+    tournamentId: { type: Schema.ObjectId, required: true, ref: 'Tournament' },
+    leagueName: { type: String, required: true, minlength: 3, maxlength: 25 },
+    stableCapacity: { type: Number, required: true, min: 0, max: 10 },
+    playerCapacity: { type: Number, required: true, min: 3, max: 10 },
+    startDate: { type: Date, required: true },
+    public: { type: Boolean, required: true, default: false },
+    players: { type: Array, required: true }
+},
+    { timestamps: true, toJSON: { virtuals: true } })
+
+LeagueSchema.virtual('Tournament', {
+    localField: 'tournamentId',
+    ref: 'Tournament',
+    foreignField: '_id',
+    justOne: true
+})
