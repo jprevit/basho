8a4171704703123fd3f2a5558343dac3f905ed0a
Ross Pataro <roswellpataro@gmail.com>
2024-05-01 13:56:43 -0600

    Array of selectable dates in tournament service

--- client/src/services/TournamentsService.js
+++ client/src/services/TournamentsService.js
@@ -4,10 +4,27 @@ import { sumoApi } from "./AxiosService.js"
 
 
 class TournamentsService{
+
+    availableDates = [
+    "202403",
+    "202401",
+    "202311",
+    "202309",
+    "202307",
+    "202305",
+    "202303",
+    "202301",
+    "202211",
+    "202209",
+    "202207",
+    "202205",
+    "202203",
+    "202201"]
+
     async getBashoById(){
         const datesResponse = await sumoApi.get('/bashoIds')
         // console.log("the dates 📅", datesResponse.data)
-        const bashoDateIndex = Math.floor(Math.random() * datesResponse.data.length)
+        const bashoDateIndex = Math.floor(Math.random() * this.availableDates.length)
         // console.log(datesResponse.data[bashoDateIndex])
 
         const randomBasho = await sumoApi.get(`basho/${datesResponse.data[bashoDateIndex]}/banzuke/Makuuchi`)
