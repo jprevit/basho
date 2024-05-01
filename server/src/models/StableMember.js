11a4bbbab5b889f33ad6f21d38120fa6e96449c6
John Previtera <johngprevitera@gmail.com>
2024-05-01 13:54:55 -0600

    stablemember model complete

---
+++ server/src/models/StableMember.js
@@ -0,0 +1,28 @@
+import { Schema } from "mongoose";
+
+export const StableMemberSchema = new Schema({
+    wrestlerId: { type: Schema.ObjectId, required: true, ref: 'Wrestler' },
+    leagueId: { type: Schema.ObjectId, required: true, ref: 'League' },
+    playerId: { type: Schema.ObjectId, required: true, ref: 'Player' },
+}, {
+    timestamps: true, toJSON: { virtuals: true }
+})
+
+StableMemberSchema.virtual('player', {
+    localField: 'playerId',
+    ref: 'Player',
+    foreignField: '_id',
+})
+
+StableMemberSchema.virtual('wrestler', {
+    localField: 'wrestlerId',
+    ref: 'Wrestler',
+    foreignField: '_id',
+    justOne: true
+})
+
+StableMemberSchema.virtual('league', {
+    localField: 'leagueId',
+    ref: 'League',
+    foreignField: '_id',
+})
