6fb962c13a2bd3a6166c2ce58d3f4e10a9a7b0b6
John Previtera <johngprevitera@gmail.com>
2024-05-02 15:16:03 -0600

    updated league schema

--- server/src/models/League.js
+++ server/src/models/League.js
@@ -4,7 +4,7 @@ export const LeagueSchema = new Schema({
     creatorId: { type: Schema.ObjectId, required: true, ref: 'Account' },
     tournamentId: { type: String, required: true },
     leagueName: { type: String, required: true, minlength: 3, maxlength: 25 },
-    stableCapacity: { type: Number, required: true, min: 0, max: 10 },
+    stableCapacity: { type: Number, required: true, min: 0, max: 10, default: 5 },
     playerCapacity: { type: Number, required: true, min: 3, max: 10 },
     startDate: { type: Date, required: true, default: new Date() },
     public: { type: Boolean, required: true, default: false },
