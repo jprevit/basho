746e17f8a762498e40803173a81a724b893a1a45
John Previtera <johngprevitera@gmail.com>
2024-05-02 11:47:31 -0600

    active league page mocked completely with placeholder content

--- client/src/pages/ActiveLeaguePage.vue
+++ client/src/pages/ActiveLeaguePage.vue
@@ -1,4 +1,6 @@
 <script setup>
+import PlayerHead from '../components/PlayerHead.vue';
+
 
 </script>
 
@@ -9,19 +11,20 @@
             <img src="src\assets\img\sumo-around-ring.jpg" alt="sumo wrestlers standing in circle around a ring"
                 class="banner_img p-0">
         </section>
-        <section class="row justify-content-center text-center">
-            <div class="col">
-                <h3>Jiggle Brothers</h3>
-                <div>Created 12 May 2024</div>
-            </div>
 
-        </section>
-        <section class="row justify-content-around">
+        <section class="row justify-content-around mt-2">
             <div class="col-2">
 
             </div>
             <div class="col-8">
-                <div class="row justify-content-between align-items-center">
+                <section class="row mt-2">
+                    <div class="col-6">
+                        <h3>Jiggle Brothers</h3>
+                        <div>Created 12 May 2024</div>
+                    </div>
+
+                </section>
+                <div class="row mt-5 justify-content-between align-items-center">
                     <div class="col-4">
                         <h1 class="m-0 p-0">Invite</h1>
                     </div>
@@ -30,13 +33,25 @@
                     </div>
                     <hr />
                     <div class="col-12">
-                        head goes here
+                        <div class="row mt-2">
+                            <PlayerHead class="col-2" />
+                            <PlayerHead class="col-2" />
+                            <PlayerHead class="col-2" />
+                            <PlayerHead class="col-2" />
+                            <PlayerHead class="col-2" />
+                        </div>
+                        <div class="row justify-content-end my-3">
+                            <button class="btn btn-mainblue rounded-pill col-3 text-light fw-bold">Start
+                                Drafting!</button>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col">
-                Invite Code: 12344fd
-                <button class="btn btn-red">Close Room</button>
+            <div class="col-2">
+                <div class="text-end mt-2">
+                    <p>Invite Code: 12344fd</p>
+                    <p role="button" class="text-red">X Close Room</p>
+                </div>
             </div>
         </section>
 
@@ -47,8 +62,8 @@
 <style lang="scss" scoped>
 .banner_img {
     object-fit: cover;
-    object-position: 50% 30%;
-    max-height: 40vh;
+    object-position: 50% 40%;
+    max-height: 30vh;
     filter: contrast(50%);
 }
 </style>
