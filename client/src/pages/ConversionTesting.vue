f9e79f760fa30204c326bbd8bb5c961ae467aa8d
masonspacestation <71409121+masonspacestation@users.noreply.github.com>
2024-05-06 11:38:33 -0600

    working to merge wrestlers from different sources

--- client/src/pages/ConversionTesting.vue
+++ client/src/pages/ConversionTesting.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+import WrestlerConversionForm from "../components/WrestlerConversionForm.vue";
 </script>
 
 
@@ -7,10 +7,11 @@
   <div class="container">
     <div class="row">
       <button style="border-radius: 25px;" class="btn btn-grey border border-5 border-lightblue fs-2  w-75 py-3 mt-3 "
-        data-bs-toggle="modal" title="Convert Wrestlers" data-bs-target="#convertWrestlerModal">Get Wrestlers to
+        data-bs-toggle="modal" title="Convert Wrestlers" data-bs-target="#convertWrestlersModal">Get Wrestlers to
         Convert</button>
     </div>
   </div>
+  <WrestlerConversionForm />
 </template>
 
 
