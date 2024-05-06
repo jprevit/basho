5d09a06e7b08a2a423ec7bc4951241ea28124730
Khile <khilek@gmail.com>
2024-05-06 15:54:23 -0600

    Set Active Tournament works and changed function name to setupLeaguePage

--- client/src/models/Wrestler.js
+++ client/src/models/Wrestler.js
@@ -2,18 +2,19 @@
 
 
 
-export class Wrestler{
-  constructor(data){
+export class Wrestler {
+  constructor(data) {
     this.id = data.id
-      this.name = data.name,
-      this.currentRank = data.currentRank
-      this.stable = data.stable
-      this.height = data.height
-      this.weight = data.weight
-      this.winsByDivision = data.winsByDivision
-      this.lossByDivision = data.lossByDivision
-      this.birthDate = data.birthDate
-      this.debut = data.debut
+    this.name = data.name
+    this.currentRank = data.currentRank
+    this.stable = data.stable
+    this.height = data.height
+    this.weight = data.weight
+    this.winsByDivision = data.winsByDivision
+    this.lossByDivision = data.lossByDivision
+    this.birthDate = data.birthDate
+    this.debut = data.debut
+
   }
 }
 
