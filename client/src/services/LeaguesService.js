a24556c3f4b77ac041e40840a094b6182c53a438
John Previtera <johngprevitera@gmail.com>
2024-05-06 11:35:12 -0600

    changes to league finding

--- client/src/services/LeaguesService.js
+++ client/src/services/LeaguesService.js
@@ -14,27 +14,28 @@ class LeaguesService {
     await tournamentsService.getBashoById()
     leagueData.tournamentId = AppState.activeTournament.bashoId
     const response = await api.post(`api/leagues`, leagueData)
-    console.log('🦒', response.data);
+    // console.log('🦒', response.data);
     const newLeague = new League(response.data)
     AppState.leagues.push(newLeague)
     AppState.activeLeague = newLeague
-    console.log('active tourn 1', AppState.activeTournament);
-    console.log('new league 2', newLeague);
+    // console.log('active tourn 1', AppState.activeTournament);
+    // console.log('new league 2', newLeague);
 
     router.push({ name: 'ActiveLeague', params: { leagueId: AppState.activeLeague.id } })
-    console.log('New league probably created', newLeague);
+    console.log('league created', newLeague);
 
   }
 
   async getLeagueById(leagueId) {
     const response = await api.get(`api/leagues/${leagueId}`)
-    console.log('found league', response.data);
+    // console.log('found league', response.data);
     return response
   }
 
   async joinLeagueById(leagueId){
     console.log('join league form info',leagueId)
     const response = await this.getLeagueById(leagueId)
+    AppState.activeLeague = response.data
     await api.post(`api/leagues/${leagueId}`, leagueId)
     router.push(`activeLeague/${leagueId}`)
     return response
