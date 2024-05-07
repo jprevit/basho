62fbc598f81531e9074f3f9ee63ddabd5445b930
Ross Pataro <roswellpataro@gmail.com>
2024-05-07 11:07:53 -0600

    Changed min player count for league to 2 Prevent join at server level if capacity is full or league has started

--- server/src/services/PlayersService.js
+++ server/src/services/PlayersService.js
@@ -1,6 +1,6 @@
 import { dbContext } from "../db/DbContext.js"
 import { logger } from "../utils/Logger.js"
-import { logger } from "../utils/Logger.js"
+import { leaguesService } from "./LeaguesService.js"
 
 
 
@@ -11,8 +11,13 @@ class PlayersService {
         const existingPlayer = await dbContext.Players.findOne({ accountId: newPlayerData.accountId, leagueId: newPlayerData.leagueId })
         if (existingPlayer) throw new Error("You have already joined this league.")
 
+        const leagueAttemptingToJoin = await leaguesService.getLeagueById(newPlayerData.leagueId)
+        if (leagueAttemptingToJoin.players.length >= leagueAttemptingToJoin.playerCapacity) throw new Error("This league is full")
+        if (leagueAttemptingToJoin.state != "starting") throw new Error("This league has already started")
+
         logger.log('service received', newPlayerData)
         const newPlayer = await dbContext.Players.create(newPlayerData)
+        leagueAttemptingToJoin.players.push(newPlayerData.accountId)
         // newPlayer.populate('league')
         return newPlayer
     }
